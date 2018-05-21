452d66086a6ffdb04d0790fd15d07e911b649123
Eisah Jones <eisah40@gmail.com>
2018-05-21 16:35:41 -0700

    Modified trail effects; Blinking arrows for elevators

---
+++ DodgeBall2D/Assets/BlinkArrows.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkArrows : MonoBehaviour {
+
+    public Sprite arrowOff;
+    public Sprite arrowOn;
+
+    private List<GameObject> arrows;
+
+	// Use this for initialization
+	void Start () {
+        arrows = new List<GameObject>();
+        foreach (Transform child in transform)
+        {
+            arrows.Add(child.gameObject);
+        }
+        StartCoroutine( blink() );	
+	}
+
+    private IEnumerator blink(){
+        WaitForSeconds waitTime = new WaitForSeconds(0.25f);
+        WaitForSeconds resetTime = new WaitForSeconds(0.5f);
+        while (true){
+            yield return resetTime;
+            foreach (GameObject arrow in arrows){
+                
+                arrow.GetComponent<SpriteRenderer>().sprite = arrowOn;
+                yield return waitTime;
+            }
+            yield return resetTime;
+            foreach (GameObject arrow in arrows)
+            {
+                arrow.GetComponent<SpriteRenderer>().sprite = arrowOff;
+            }
+        }
+    }
+}
